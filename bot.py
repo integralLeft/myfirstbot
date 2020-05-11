2da88a829c9b161fa19028e0e61e2154a34c478c
Ben Rosenberg <xethrog42@gmail.com>
2020-05-10 23:58:55 -0400

    message thing

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+# below: manual input of token/guild vals
 #TOKEN = input('Please enter the token: ')
 #GUILD = input('Please enter the guild: ')
 
@@ -15,12 +16,24 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-     guild = discord.utils.get(client.guilds, name=GUILD)
-
-
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
 )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    cmddict = {'greet':'Hi, ' + message.author + '!', 'gay':'no u'}
+    prefix = 'm!'
+    if message.content[:len(prefix)] == prefix:
+        body = message.content[len(prefix)]
+        if body not in cmddict:
+            response = 'enter a valid command dumbass'
+        else:
+            response = cmddict[body]
+        await message.channel.send(response)
 
 client.run(TOKEN)
